6f111dab453b8195d19a60c8770b6ff61d9d7df9
connosieurofdoom <shreyasajitrajendran@gmail.com>
2020-03-24 10:14:47 +0530

    Profiler update

---
+++ profiler/__init__.py
@@ -0,0 +1,16 @@
+
+import os
+import psutil
+from timeit import default_timer as timer
+
+def profile(func,param):
+    print("Input:",param)
+    start = timer()
+    print("Output:",func(param))
+    print("Execution Time:", timer()-start)
+
+
+    py = psutil.Process(os.getpid())
+    memoryUse = py.memory_info()[0]/2.**30
+    print("MemoryUsage:",memoryUse,"\ncpuUsage:",psutil.cpu_percent(),"\nVirtualMemory:",psutil.virtual_memory())
+
